95667d29aa5285d0c4494da03160ff0b45e7af2e
Thanakrit Promsiri <thanakrit.promsiri@stream.co.th>
2015-10-27 19:46:16 +0700

    init project

---
+++ RomanizedSanskritThai/src/sanskrit/romanized/thai/RomanizedSanskritThai.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sanskrit.romanized.thai;
+
+/**
+ *
+ * @author Thanakrit.P
+ */
+public class RomanizedSanskritThai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
